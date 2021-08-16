628b04880c6b53ec3a7c33506fb91a6ea4c29113
Tank <johnson90.cm@gmail.com>
2021-08-16 08:49:04 -0600

    setup psuedocode notes for project

---
+++ Script.js
@@ -0,0 +1,56 @@
+// HTML:
+// View Highscores in header
+// Time Countdown in header
+// Rules of the quiz
+// Start Button
+// Input for name to record highscore
+// Button to submit
+// Button to clear highscore
+
+// CSS:
+// Style Header
+// Style Timer
+// Style Buttons
+
+// JS:
+
+// vars
+// Create Vars that target DOM elements
+// quiz timer
+// var "var = quizTimerCounter = 60" 
+// questions index
+// var "var questions = []" 
+// document selectors
+// var "var = document.querySelector(#quiz-question-1")" * 4
+
+// functions
+// function to handle website timer
+// function to start quiz
+// function to generate questions
+// function to move to next set of questions array
+// function to log to local storage
+// function to pull from local storage
+// funtion to clear local storage
+
+
+
+// var tasks = [
+//     {
+//       id: 1,
+//       name: "Add localStorage persistence",
+//       type: "Web",
+//       status: "in progress"
+//     },
+//     {
+//       id: 2,
+//       name: "Learn JavaScript",
+//       type: "Web",
+//       status: "in progress"
+//     },
+//     {
+//       id: 3,
+//       name: "Refactor code",
+//       type: "Web",
+//       status: "to do"
+//     }
+//   ];
